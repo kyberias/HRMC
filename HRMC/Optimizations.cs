f3dffe5372edfa5d9254e6285e21f4107f5f97ce
Jaakko Haakana <jaakko@haakana.net>
2016-07-04 21:25:07 +0300

    More optimizations

--- HRMC/Optimizations.cs
+++ HRMC/Optimizations.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HRMC
 {
@@ -10,10 +7,135 @@ namespace HRMC
     {
         public static IEnumerable<CodeGenerator.Instruction> Optimize(IList<CodeGenerator.Instruction> instructions)
         {
-            return OptimizeCopyFrom(instructions);
+            return
+                CombineMultipleLabelsIntoOne(
+                RemoveInstructionsAfterJump(
+                OptimizeBumpUpCopyFrom(
+                OptimizeCopyFrom(instructions).ToList()).ToList()).ToList());
         }
 
-        static IEnumerable<CodeGenerator.Instruction> OptimizeCopyFrom(IList<CodeGenerator.Instruction> instructions)
+        /*    
+         Any instructions following an unconditional jump without any labels in between can be removed.
+         JUMP u
+         JUMP w
+        */
+
+        public static IEnumerable<CodeGenerator.Instruction> RemoveInstructionsAfterJump(IList<CodeGenerator.Instruction> instructions)
+        {
+            bool wasJump = false;
+            for (int i = 0; i < instructions.Count; i++)
+            {
+                if (!wasJump && instructions[i].Opcode == CodeGenerator.Opcode.Jump)
+                {
+                    yield return instructions[i];
+                    wasJump = true;
+                    continue;
+                }
+
+                if (wasJump)
+                {
+                    switch (instructions[i].Opcode)
+                    {
+                        case CodeGenerator.Opcode.Label:
+                            wasJump = false;
+                            break;
+                    }
+
+                    if (!wasJump)
+                    {
+                        yield return instructions[i];
+                    }
+                }
+                else
+                {
+                    yield return instructions[i];
+                }
+            }
+        }
+
+        /*
+         Multiple labels can be combined into one label:
+            o:
+            q:
+        */
+
+        static IEnumerable<CodeGenerator.Instruction> CombineMultipleLabelsIntoOne(
+            IList<CodeGenerator.Instruction> instructions)
+        {
+            Dictionary<string, string> removedLabels = new Dictionary<string, string>();
+            List<CodeGenerator.Instruction> newList = new List<CodeGenerator.Instruction>();
+
+            CodeGenerator.Instruction prevInstruction = null;
+
+            foreach (var i in instructions)
+            {
+                if (i.Opcode == CodeGenerator.Opcode.Label)
+                {
+                    if (prevInstruction == null)
+                    {
+                        prevInstruction = i;
+                        newList.Add(i);
+                    }
+                    else
+                    {
+                        removedLabels.Add(i.TextOperand, prevInstruction.TextOperand);
+                    }
+                }
+                else
+                {
+                    prevInstruction = null;
+                    newList.Add(i);
+                }
+            }
+
+            // Fix Jumps
+            foreach (var rl in removedLabels)
+            {
+                foreach(var jump in newList.Where(
+                    i => (i.Opcode == CodeGenerator.Opcode.Jump || i.Opcode == CodeGenerator.Opcode.JumpN || i.Opcode == CodeGenerator.Opcode.JumpZ)
+                    && i.TextOperand == rl.Key))
+                {
+                    jump.TextOperand = rl.Value;
+                }
+            }
+            return newList;
+        }
+
+        /*
+         Labels preceding a JUMP instructions can be removed and the JUMP origins replaced
+         by the remaining JUMP's target. 
+         
+        v:
+        w:
+            JUMP b
+        */
+
+        /*    
+         BUMPUP 19
+        COPYFROM 19
+        */
+
+        static IEnumerable<CodeGenerator.Instruction> OptimizeBumpUpCopyFrom(IList<CodeGenerator.Instruction> instructions)
+        {
+            for (int i = 0; i < instructions.Count; i++)
+            {
+                if (i > 0 && instructions[i].Opcode == CodeGenerator.Opcode.CopyFrom
+                    &&
+                    (instructions[i - 1].Opcode == CodeGenerator.Opcode.BumpDn ||
+                     instructions[i - 1].Opcode == CodeGenerator.Opcode.BumpUp)
+                    && instructions[i].Operand == instructions[i - 1].Operand)
+                {
+
+                }
+                else
+                {
+                    yield return instructions[i];
+                }
+            }
+        }
+
+
+            static IEnumerable<CodeGenerator.Instruction> OptimizeCopyFrom(IList<CodeGenerator.Instruction> instructions)
         {
             /*
     COPYFROM 24
